fa95e83d26dd6966217f69101af1f84fe0e5695b
Chandan Singh <chandan.devel@gmail.com>
2015-08-08 02:04:08 +0530

    Use flickr url instead of youtube for testing

--- scripts/composer-update.sh
+++ scripts/composer-update.sh
@@ -6,7 +6,6 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
-cd "$DRUPAL_TI_DRUPAL_DIR"
-cd core
+cd "$DRUPAL_TI_DRUPAL_DIR/core"
 composer drupal-rebuild
 composer update embed --prefer-source
--- src/Tests/UrlEmbedFilterTest.php
+++ src/Tests/UrlEmbedFilterTest.php
@@ -21,26 +21,26 @@ class UrlEmbedFilterTest extends UrlEmbedTestBase {
    * are passed. Also tests situations when embed fails.
    */
   public function testFilter() {
-    // Tests url embed using sample youtube url.
+    // Tests url embed using sample flickr url.
     $content = '<div data-embed-url="' . $this->sample_url . '">This placeholder should not be rendered.</div>';
     $settings = array();
     $settings['type'] = 'page';
-    $settings['title'] = 'Test url embed with sample youtube url';
+    $settings['title'] = 'Test url embed with sample flickr url';
     $settings['body'] = array(array('value' => $content, 'format' => 'custom_format'));
     $node = $this->drupalCreateNode($settings);
     $this->drupalGet('node/' . $node->id());
-    $this->assertRaw('<iframe width="480" height="270" src="https://www.youtube.com/embed/7ipydm8guz4?feature=oembed" frameborder="0" allowfullscreen=""></iframe>');
-    $this->assertNoRaw('Placeholder does not appears in the output when embed is successful.');
+    $this->assertRaw('<a data-flickr-embed="true" href="https://www.flickr.com/photos/bees/2341623661/" title="ZB8T0193 by ‮‭‬bees‬, on Flickr"><img src="https://farm4.staticflickr.com/3123/2341623661_7c99f48bbf_b.jpg" width="1024" height="683" alt="ZB8T0193" /></a>');
+    $this->assertNoRaw('This placeholder should not be rendered.', 'Placeholder does not appears in the output when embed is successful.');
 
     // Test that tag of container element is replaced when it's 'drupal-url'.
     $content = '<drupal-url data-embed-url="' . $this->sample_url . '">this placeholder should not be rendered.</drupal-url>';
     $settings = array();
     $settings['type'] = 'page';
-    $settings['title'] = 'test url embed with entity-id and view-mode';
+    $settings['title'] = 'Test url embed with sample flickr url';
     $settings['body'] = array(array('value' => $content, 'format' => 'custom_format'));
     $node = $this->drupalCreateNode($settings);
     $this->drupalget('node/' . $node->id());
-    $this->assertRaw('<iframe width="480" height="270" src="https://www.youtube.com/embed/7ipydm8guz4?feature=oembed" frameborder="0" allowfullscreen=""></iframe>');
+    $this->assertRaw('<a data-flickr-embed="true" href="https://www.flickr.com/photos/bees/2341623661/" title="ZB8T0193 by ‮‭‬bees‬, on Flickr"><img src="https://farm4.staticflickr.com/3123/2341623661_7c99f48bbf_b.jpg" width="1024" height="683" alt="ZB8T0193" /></a>');
     $this->assertNoRaw('</drupal-url>');
 
     // Test that tag of container element is not replaced when it's not
@@ -48,11 +48,11 @@ class UrlEmbedFilterTest extends UrlEmbedTestBase {
     $content = '<not-drupal-url data-embed-url="' . $this->sample_url . '">this placeholder should not be rendered.</not-drupal-url>';
     $settings = array();
     $settings['type'] = 'page';
-    $settings['title'] = 'test url embed with entity-id and view-mode';
+    $settings['title'] = 'Test url embed with sample flickr url';
     $settings['body'] = array(array('value' => $content, 'format' => 'custom_format'));
     $node = $this->drupalCreateNode($settings);
     $this->drupalget('node/' . $node->id());
-    $this->assertRaw('<iframe width="480" height="270" src="https://www.youtube.com/embed/7ipydm8guz4?feature=oembed" frameborder="0" allowfullscreen=""></iframe>');
+    $this->assertRaw('<a data-flickr-embed="true" href="https://www.flickr.com/photos/bees/2341623661/" title="ZB8T0193 by ‮‭‬bees‬, on Flickr"><img src="https://farm4.staticflickr.com/3123/2341623661_7c99f48bbf_b.jpg" width="1024" height="683" alt="ZB8T0193" /></a>');
     $this->assertRaw('</not-drupal-url>');
   }
 
