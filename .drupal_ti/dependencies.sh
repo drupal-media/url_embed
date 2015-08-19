04e4bbf43a32890aa32f9780a35b5588eeee7159
Dave Reid <dave@davereid.net>
2015-08-19 10:24:01 -0500

    Use git checkouts for dependencies

--- .drupal_ti/dependencies.sh
+++ .drupal_ti/dependencies.sh
@@ -6,9 +6,13 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
-cd "$DRUPAL_TI_DRUPAL_DIR"
-drush dl embed --yes
-drush pm-enable embed --yes
-drush dl composer_manager --yes
+# Download dependencies
+mkdir -p "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
+cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
+git clone --depth 1 --branch 8.x-1.x https://github.com/drupal-media/embed.git
+git clone --depth 1 --branch 8.x-1.x http://git.drupal.org/project/composer_manager.git
+
+# Initialize composer manager
 drush pm-enable composer_manager --yes
-drush composer-manager-init
+cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH/composer_manager"
+php scripts/init.php
